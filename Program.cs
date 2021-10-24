005c1556d8ec883c0ea970defc8027c8ebe1ea50
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-10-24 11:39:44 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Digite um número:");
+            n=Convert.ToInt32(Console.ReadLine());
+
+             if (n < 0)
+            {
+                Console.ForegroundColor=ConsoleColor.DarkRed;
+                Console.WriteLine("Negativo");
+                Console.ResetColor();
+
+                }
+            else if (n == 0)
+            {
+                Console.WriteLine("Zero");
+            }
+            else if (n > 0)
+            {
+                Console.ForegroundColor=ConsoleColor.DarkGreen;
+                Console.WriteLine ("Positivo");
+                Console.ResetColor();
+            }    
+        }
+    }
+}
